fe1d5abb7ae03c882d4457ecf19b7a168536512f
Okumura Fumiko Santana <43951246+extgfx@users.noreply.github.com>
2020-08-29 15:31:51 +0700

    STOP USING MANUAL SENDER

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ wa.create().then(client => start(client));
 
 function start(client) {
   client.onMessage(message => {
-    const rand = Math.floor(Math.random() * (19377262 - 17390163 + 1)) + 17390163;
+    const rand = Math.floor(Math.random() * (119613999 - 119613000 + 1)) + 119613000;
     client.sendText('6281319990822@c.us',`Rexona ${rand}`)
     console.log(`Rexona ${rand} Sent. Waiting for another..`)
   });
